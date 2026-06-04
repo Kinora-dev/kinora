994854680377b714bd6278f7452de25fad356591
Joris Gallot <joris.gallot18@gmail.com>
2026-06-04 20:38:52 +0200

    chore: theme update storageKey

--- src/composables/useTheme.ts
+++ src/composables/useTheme.ts
@@ -2,5 +2,4 @@ import { useColorMode } from '@vueuse/core'
 
 export type ColorMode = 'auto' | 'light' | 'dark'
 
-// auto = follow system; persisted to localStorage by useColorMode.
-export const colorMode = useColorMode({ emitAuto: true })
+export const colorMode = useColorMode({ emitAuto: true, storageKey: 'playback-theme' })
