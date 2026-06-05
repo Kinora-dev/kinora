ad0535f55d702c47de73861a499202e6d7f529e0
Joris Gallot <joris.gallot18@gmail.com>
2026-06-06 00:32:58 +0200

    fix: resizable handle for panel source, call, etc

--- packages/ui/src/components/ui/resizable/ResizableHandle.vue
+++ packages/ui/src/components/ui/resizable/ResizableHandle.vue
@@ -17,7 +17,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
     data-slot="resizable-handle"
     v-bind="forwarded"
     :class="cn(
-      'relative flex w-px items-center justify-center bg-border transition-colors after:absolute after:inset-y-0 after:left-1/2 after:w-2 after:-translate-x-1/2 hover:bg-signal/60 focus-visible:ring-1 focus-visible:ring-ring focus-visible:outline-none data-[state=drag]:bg-signal data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-2 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0',
+      'relative flex w-px items-center justify-center bg-border transition-colors after:absolute after:inset-y-0 after:left-1/2 after:w-2 after:-translate-x-1/2 hover:bg-signal/60 focus-visible:ring-1 focus-visible:ring-ring focus-visible:outline-none data-[state=drag]:bg-signal data-[orientation=vertical]:h-px data-[orientation=vertical]:w-full data-[orientation=vertical]:after:left-0 data-[orientation=vertical]:after:h-2 data-[orientation=vertical]:after:w-full data-[orientation=vertical]:after:-translate-y-1/2 data-[orientation=vertical]:after:translate-x-0 [&[data-orientation=vertical]>div]:rotate-90',
       props.class,
     )"
   >
