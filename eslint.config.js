fc3207b1217f569bfd89abd52a29c842f13d9cf0
Joris Gallot <joris.gallot18@gmail.com>
2026-06-08 13:31:12 +0200

    chore: fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,12 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   vue: true,
   typescript: true,
-  // Vendored from microsoft/playwright (Apache-2.0): don't lint upstream code.
+  astro: true,
   ignores: [
+    '**/dist/**',
+    '**/node_modules/**',
+    'landing/.astro',
+    // Vendored from microsoft/playwright (Apache-2.0): don't lint upstream code.
     'packages/trace-viewer/src/core/**',
     'packages/trace-viewer/src/sw/**',
     'packages/trace-viewer/src/sw-main.ts',
