3f80bf0f126c016fe3fd0cbf0f4348ec1426dee9
Joris Gallot <70059224+joris-gallot@users.noreply.github.com>
2026-06-10 18:52:13 +0200

    chore: add web dockerfile (#5)

--- packages/server/src/lib/utils.ts
+++ packages/server/src/lib/utils.ts
@@ -2,7 +2,7 @@ import { env } from './env'
 
 export function getTrustedOrigins(): string[] {
   const byEnv: Record<typeof env.NODE_ENV, string[]> = {
-    production: ['https://kinora.dev'],
+    production: ['https://app.kinora.dev'],
     development: ['http://localhost:5173'],
   }
   return byEnv[env.NODE_ENV]
